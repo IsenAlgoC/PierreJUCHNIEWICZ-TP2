7e6b7b26e682b36d51319cd4dc5cb663d9f0ecc5
Pierre JUCHNIEWICZ <pierre.juchniewicz@hei.yncrea.fr>
2020-09-25 09:59:03 +0200

    Exercice 1 terminé

--- TP2/Exo1/Boucle.c
+++ TP2/Exo1/Boucle.c
@@ -1,35 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	// Initialisation des variables
-	int n = 100;
-	int compteurWhile = 2;
-	int compteurDoWhile = 2;
-	int valSommeFor = 0;			//valeur de la somme de la boucle for
-	int valSommeWhile = 0;			//valeur de la somme de la boucle while
-	int valSommeDoWhile = 0;		//valeur de la somme de la bouche DoWhile
+void boucleFor(int n) {
 
-	// Utilisation des 3 boucles différentes
+	unsigned short valSommeFor = 0;			//valeur de la somme de la boucle for
 
-	for (int i = 2; i <= n; ++i) {
+	for (int i = 1; i <= n; ++i) {
 		valSommeFor = valSommeFor + i;
 	}
-	while (compteurWhile <= 100) {
+	printf("Votre somme de votre boucle for est : %d pour les %d premiers entiers positifs !\n", valSommeFor, n);
+}
+
+void boucleWhile(int n) {
+
+	unsigned short valSommeWhile = 0;			//valeur de la somme de la boucle while
+	int compteurWhile = 1;
+
+	while (compteurWhile <= n) {
 		valSommeWhile = valSommeWhile + compteurWhile;
 		++compteurWhile;
 	}
-	do {
-		++compteurDoWhile;
-		valSommeDoWhile = valSommeDoWhile + compteurDoWhile;
-	} while (compteurDoWhile <= 100);
 
-	// Affichage des résultats des 3 boucles
-
-	printf("Votre somme de votre boucle for est : %d pour les %d premiers entiers positifs !\n", valSommeFor, n);
 	printf("Votre somme de votre boucle while est : %d pour les %d premiers entiers positifs !\n", valSommeWhile, n);
-	printf("Votre somme de votre boucle do/while est : %d pour les %d premiers entiers positifs !\n", valSommeDoWhile, n);
-	system("PAUSE");
-	return 0;
+
 }
 
+void boucleDoWhile(int n) {
+
+	unsigned short valSommeDoWhile = 0;		//valeur de la somme de la bouche DoWhile
+	int compteurDoWhile = 1;
+
+	do {
+		valSommeDoWhile = valSommeDoWhile + compteurDoWhile;
+		compteurDoWhile++;
+	} while (compteurDoWhile < n + 1);
+
+	printf("Votre somme de votre boucle do/while est : %d pour les %d premiers entiers positifs !\n", valSommeDoWhile, n);
+}
+
+void valeurAvantDepassement() {
+	int sommemax = 0;
+	int i = 0;
+	int exit = 0;
+	while (exit == 0) {
+		i++;
+		sommemax = sommemax + i;
+		if (sommemax + i >= 65535) {
+			65535 - sommemax <= i;
+			exit = 1;
+		}
+	}
+	printf("\nLa valeur max de n avant depassement unsigned short est : %d \n", i);
+}
+
+int main() {
+	int recommencer = 1;
+	while (recommencer) {
+		int n;
+		printf("Saisissez votre valeur de n : ");
+		scanf_s("%d", &n);
+		if (n <= 361) {
+			printf("\nVous avez saisi n = %d\n", n);
+			// Utilisation des 3 boucles
+			boucleFor(n);
+			boucleWhile(n);
+			boucleDoWhile(n);
+			valeurAvantDepassement();
+			recommencer = 0;
+		}
+		else {
+			printf("\nLa valeur saisie n'est pas correcte\nRe-");
+		}
+	}
+}
+
+// La somme est représentée par une variable de type unsigned short int, quelle est la valeur maximum de n pour que la variable représentant la somme ait une valeur correcte ?
+// --> Réponse : la valeur maximum d'un unsigned short est 65 535, or, si n = 361, nous avons comme résultat de somme 65 341, dernier nombre possible avant dépassement, donc nous devons rentrer un n <= 361.
